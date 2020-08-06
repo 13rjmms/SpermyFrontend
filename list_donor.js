c6f6e3c78664c8a0d19ad71bfd66ecf40ebd43cd
Edgar Dias <edgarus@gmail.com>
2020-08-06 18:48:46 +0100

    update signups and list

--- list_donor.js
+++ list_donor.js
@@ -16,73 +16,68 @@ function getAllDonors() {
 
 function getAllDonorsSuccess(response) {
 
-    console.log(response);
     var donorList = $('#donors');
     donorList.empty();
     
+    let uniqueNationalites = [...new Set(response.map(item => item.nacionality))];
+    console.log(uniqueNationalites);
+
+    let uniqueRaces = [...new Set(response.map(item => item.race))];
+    console.log(uniqueRaces);
+
+    let uniqueAges = [...new Set(response.map(item => item.age))];
+    console.log(uniqueAges);
+
+    let uniqueBloodTypes = [...new Set(response.map(item => item.bloodType))];
+    console.log(uniqueBloodTypes);
+
+    let uniqueHairColors = [...new Set(response.map(item => item.hairColor))];
+    console.log(uniqueHairColors);
+
+    let uniqueEyeColors = [...new Set(response.map(item => item.eyeColor))];
+    console.log(uniqueEyeColors);
+
     response.forEach(element => {
-        /* 
-        "id": 1,
-        "bi": "123123123",
-        "firstName": "Nuno",
-        "lastName": "Midget",
-        "age": 18,
-        "gender": "Male",
-        "email": "yo@m.com",
-        "phone": "123123123",
-        "address": "fucket",
-        "country": "Pt",
-        "city": "Lisboa",
-        "race": "Yellow",
-        "hairColor": "Red",
-        "eyeColor": "Blue",
-        "nacionality": "PT",
-        "weight": "90",
-        "bloodType": "A+",
-        "imgURL": "https://image.shutterstock.com/image-photo/portrait-stylish-midget-mc-headphones-260nw-1247042659.jpg",
-        "productList": [],
-        "numberSamples": 0
-     */
-        var donor = '<div class="card mb-3">' +
+    
+        var donor = '<div class="card mb-3 container-fluid">' +
                         '<div class="row no-gutters"> ' +
                             '<div class="col-md-3">' +
-                            '<img src="'+ element.imgURL+'" class="img-thumbnail border-0" alt="...">' +
+                            '<img src="'+ element.imgURL+'" class="img-thumbnail border-0" style="max-height: 200px; max-width:200px;" alt="...">' +
                             '</div>'+
                             '<div class="col-md-9">' +
                                 '<div class="card-body">'+
                                     '<div class="row">'+
                                         '<h5 class="card-title">'+ element.firstName + ' ' + element.lastName+ '</h5>' +
-                                        '<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>' +
+                                    '</div>' +
+                                    '<div class="row">'+
+                                        '<p class="card-text">'+ element.bio + '</p>' +
+                                    '</div>' +
+                                    '<div class="row mt-3">'+
+                                        '<div class="col-md-4">'+
+                                            '<p class="card-text"><small class="text-muted">Nacionality: </small>'+ element.nacionality +'</p>' +
+                                        '</div>' +
+                                        '<div class="col-md-4">'+
+                                            '<p class="card-text"><small class="text-muted">Race: </small>'+ element.race +'</p>' +
+                                        '</div>' +
+                                        '<div class="col-md-4">'+
+                                            '<p class="card-text"><small class="text-muted">Eye color: </small>'+ element.eyeColor +'</p>' +
+                                        '</div>' +
                                     '</div>' +
                                     '<div class="row">'+
                                         '<div class="col-md-4">'+
-                                            '<p class="card-text"><small class="text-muted">'+ element.nacionality +'</small></p>' +
+                                            '<p class="card-text"><small class="text-muted">Hair color: </small>'+ element.hairColor +'</p>' +
                                         '</div>' +
                                         '<div class="col-md-4">'+
-                                            '<p class="card-text"><small class="text-muted">'+ element.race +'</small></p>' +
+                                            '<p class="card-text"><small class="text-muted">Blood type: </small>'+ element.bloodType +'</p>' +
                                         '</div>' +
                                         '<div class="col-md-4">'+
-                                            '<p class="card-text"><small class="text-muted">'+ element.eyeColor +'</small></p>' +
+                                            '<p class="card-text"><small class="text-muted">Age: </small>'+ element.age +'</p>' +
                                         '</div>' +
                                     '</div>' +
-                                    '<div class="row">'+
-                                        '<div class="col-md-4">'+
-                                            '<p class="card-text"><small class="text-muted">'+ element.hairColor +'</small></p>' +
-                                        '</div>' +
-                                        '<div class="col-md-4">'+
-                                            '<p class="card-text"><small class="text-muted">'+ element.bloodType +'</small></p>' +
-                                        '</div>' +
-                                        '<div class="col-md-4">'+
-                                            '<p class="card-text"><small class="text-muted">'+ element.age +'</small></p>' +
-                                        '</div>' +
-                                    '</div>' +
-                                    '<div class="row">'+
-                                    '<div class="col-md-6">'+
-                                        '<span class="card-text">Email: '+ element.email +'</span>' +
-                                    '</div>' +
-                                    '<div class="col-md-6">'+
-                                        '<span class="card-text">Phone: '+ element.phone +'</span>' +
-                                    '</div>' +
+                                    '<div class="row mt-3">'+
+                                        '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">'+
+                                            'Connect'+
+                                        '</button>'+
                                     '</div>' +
                                     '</div>' +
                                 '</div>' +
